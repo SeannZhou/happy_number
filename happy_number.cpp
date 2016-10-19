812deb672935e5f4a21085ac6c466dbb578fee8a
Sean <sean@Admins-MBP.home>
2016-10-18 22:48:21 -0400

    C++ program for finding Happy Number!

---
+++ happy_number.cpp
@@ -0,0 +1,53 @@
+/*
+	Sean Zhou
+	Description: Happy Number 
+*/
+
+#include<iostream>
+#include<cmath>	//For squaring
+
+using namespace std;
+int main()
+{
+	int num;	//number being broken down
+	int digit;	//adds to the sum
+	int sum=0;	//check for happy number
+	int again=0;
+
+	while (again==0)
+	{
+		cout << "Please insert a number to see if it's happy or not\n";		//State the prompt
+		cin >> num;
+	
+			while ( sum!=1 && sum!= 4)			//While NOT a happy number
+				{
+					if (sum !=0)	//To stop the exchange in the beginning so num won't equal 0	
+					{ 
+						num= sum;	//To exchange the value of sum to num
+					}
+					sum= 0;
+						while ( num!=0)			//Breaking the number down
+							{	
+								digit= num%10;		
+								sum+= pow(digit,2);		
+								num= num/10;
+							}
+				}	
+
+			if (sum== 1)		//Result!!!
+				cout << "Your number is a happy number\n";
+			else if (sum== 4)
+				cout << "Your number is not a happy number\n";
+
+		cout << "Would you like to do another check?\nYes- [0]	No- [Any other number]\n";
+		cin >> again;
+	}
+	cout << "Thank You!\n";
+	return 0;
+}
+
+/*
+	Test Data:
+	Happy numbers: 7, 888
+	Unhappy numbers: 125, 71
+*/
