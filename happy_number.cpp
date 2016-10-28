c8997c01939e0b0ab9ecc511d876c3d8487d20ae
SeannZhou <seanzhou0@gmail.com>
2016-10-27 21:43:42 -0400

    Adjusted spacing

--- happy_number.cpp
+++ happy_number.cpp
@@ -7,34 +7,32 @@
 #include<cmath>	//For squaring
 
 using namespace std;
-int main()
-{
+int main() {
 	int num;	//number being broken down
 	int digit;	//adds to the sum
-	int sum=0;	//check for happy number
-	int again=0;
+	int sum = 0;	//check for happy number
+	int again = 0;
 
 		cout << "Please insert a number to see if it's happy or not\n";		//State the prompt
 		cin >> num;
 	
-			while ( sum!=1 && sum!= 4)			//While NOT a happy number
-				{
-					if (sum !=0)	//To stop the exchange in the beginning so num won't equal 0	
+			while(sum != 1 && sum != 4) {			//While NOT a happy number	
+					if(sum != 0)	//To stop the exchange in the beginning so num won't equal 0	
 					{ 
-						num= sum;	//To exchange the value of sum to num
+						num = sum;	//To exchange the value of sum to num
 					}
-					sum= 0;
-						while ( num!=0)			//Breaking the number down
+					sum = 0;
+						while(num != 0)			//Breaking the number down
 							{	
-								digit= num%10;		
-								sum+= pow(digit,2);		
-								num= num/10;
+								digit = num%10;		
+								sum += pow(digit, 2);		
+								num = num / 10;
 							}
 				}	
 
-			if (sum== 1)		//Result!!!
+			if(sum == 1)		//Result!!!
 				cout << "Your number is a happy number\n";
-			else if (sum== 4)
+			else if(sum == 4)
 				cout << "Your number is not a happy number\n";
 
 	return 0;
